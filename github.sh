28fab57a47db55dd89fd85233be25f64759a628a
Dominic Ruettimann <doxic@users.noreply.github.com>
2015-12-06 13:01:40 +0100

    initial github config

---
+++ github.sh
@@ -0,0 +1,18 @@
+## Github Config ##
+
+# Install
+sudo apt-get install -y git
+
+# Setup
+git config --global user.name "Dominic Ruettimann"
+git config --global user.email "doxic@users.noreply.github.com"
+git config --global user.email
+
+# SSH
+ssh-keygen -t rsa -b 4096 -C "doxic@users.noreply.github.com"
+/home/core/.ssh/git.id_rsa.pub
+eval $(ssh-agent -s)
+chmod 600 /home/core/.ssh/git.id_rsa
+ssh-add /home/core/.ssh/git.id_rsa
+cat /home/core/.ssh/git.id_rsa.pub
+ssh -T git@github.com
