de25ee6125398047e4ee096db09b29cc13bd1876
Dominic Rüttimann <doxic@users.noreply.github.com>
2015-12-13 15:52:59 +0100

    Update github.sh

--- github.sh
+++ github.sh
@@ -9,8 +9,7 @@ git config --global user.email "doxic@users.noreply.github.com"
 git config --global user.email
 
 # SSH
-ssh-keygen -t rsa -b 4096 -C "doxic@users.noreply.github.com"
-/home/core/.ssh/git.id_rsa.pub
+ssh-keygen -t rsa -b 4096 -C "doxic@users.noreply.github.com" -f /home/core/.ssh/git.id_rsa.pub
 eval $(ssh-agent -s)
 chmod 600 /home/core/.ssh/git.id_rsa
 ssh-add /home/core/.ssh/git.id_rsa
